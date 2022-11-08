1fda8335b791c7f2222dc03b45481ae73724b97a
Rakib Linux <rakibulinux@gmail.com>
2022-11-08 07:57:49 +0600

    slider image addes

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,28 +1,53 @@
 import React from "react";
+import fastFoodDelivery1 from "../../assets/fast-food-delivery-1.webp";
+import fastFoodDelivery2 from "../../assets/fast-food-delivery-2.webp";
 
 const Banner = () => {
   return (
-    <div className="carousel carousel-center rounded-box">
-      <div className="carousel-item">
-        <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+    <div className="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img src={fastFoodDelivery1} alt="Pizza" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
       </div>
-      <div className="carousel-item">
-        <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+      <div id="slide2" className="carousel-item relative w-full">
+        <img src={fastFoodDelivery2} alt="Pizza" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
       </div>
-      <div className="carousel-item">
-        <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+      <div id="slide3" className="carousel-item relative w-full">
+        <img src={fastFoodDelivery1} alt="Pizza" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
       </div>
-      <div className="carousel-item">
-        <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
-      </div>
-      <div className="carousel-item">
-        <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
-      </div>
-      <div className="carousel-item">
-        <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
-      </div>
-      <div className="carousel-item">
-        <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+      <div id="slide4" className="carousel-item relative w-full">
+        <img src={fastFoodDelivery2} alt="Pizza" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
       </div>
     </div>
   );
