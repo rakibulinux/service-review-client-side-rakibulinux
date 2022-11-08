56117b6f645061bb65d412f5ee4a944a8e84d16f
Rakib Linux <rakibulinux@gmail.com>
2022-11-08 07:36:43 +0600

    error page added

--- src/Routers/Routers.js
+++ src/Routers/Routers.js
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../components/ErrorPage/ErrorPage";
 import Main from "../layout/Main";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
---
+++ src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import errorPage404 from "../../assets/errorPage.png";
+const ErrorPage = () => {
+  return (
+    <section className="flex items-center h-screen p-16 bg-green-100 text-green-250">
+      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+        <img className="h-96" src={errorPage404} alt="" />
+        <div className="max-w-md text-center">
+          <p className="text-2xl font-semibold md:text-3xl mb-8">
+            Sorry, we couldn't find this page.
+          </p>
+          <Link
+            to="/"
+            className="px-8 py-3 font-semibold rounded bg-green-250 hover:bg-green-350 text-green-100"
+          >
+            Back to homepage
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ErrorPage;
