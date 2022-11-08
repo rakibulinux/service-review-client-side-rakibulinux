8df95933217c844ecc4f10620acab6a7658509e2
Rakib Linux <rakibulinux@gmail.com>
2022-11-08 08:04:42 +0600

    slider image size and color changed

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,25 +4,37 @@ import fastFoodDelivery2 from "../../assets/fast-food-delivery-2.webp";
 
 const Banner = () => {
   return (
-    <div className="carousel w-full">
+    <div className="carousel w-full max-h-96">
       <div id="slide1" className="carousel-item relative w-full">
         <img src={fastFoodDelivery1} alt="Pizza" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">
+          <a
+            href="#slide4"
+            className="btn btn-circle bg-green-250 hover:bg-green-350 border-none"
+          >
             ❮
           </a>
-          <a href="#slide2" className="btn btn-circle">
+          <a
+            href="#slide2"
+            className="btn btn-circle bg-green-250 hover:bg-green-350 border-none"
+          >
             ❯
           </a>
         </div>
       </div>
       <div id="slide2" className="carousel-item relative w-full">
-        <img src={fastFoodDelivery2} alt="Pizza" />
+        <img src={fastFoodDelivery2} alt="Burger" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
+          <a
+            href="#slide1"
+            className="btn btn-circle bg-green-250 hover:bg-green-350 border-none"
+          >
             ❮
           </a>
-          <a href="#slide3" className="btn btn-circle">
+          <a
+            href="#slide3"
+            className="btn btn-circle bg-green-250 hover:bg-green-350 border-none"
+          >
             ❯
           </a>
         </div>
@@ -30,21 +42,33 @@ const Banner = () => {
       <div id="slide3" className="carousel-item relative w-full">
         <img src={fastFoodDelivery1} alt="Pizza" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
+          <a
+            href="#slide2"
+            className="btn btn-circle bg-green-250 hover:bg-green-350 border-none"
+          >
             ❮
           </a>
-          <a href="#slide4" className="btn btn-circle">
+          <a
+            href="#slide4"
+            className="btn btn-circle bg-green-250 hover:bg-green-350 border-none"
+          >
             ❯
           </a>
         </div>
       </div>
       <div id="slide4" className="carousel-item relative w-full">
-        <img src={fastFoodDelivery2} alt="Pizza" />
+        <img src={fastFoodDelivery2} alt="Burger" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
+          <a
+            href="#slide3"
+            className="btn btn-circle bg-green-250 hover:bg-green-350 border-none"
+          >
             ❮
           </a>
-          <a href="#slide1" className="btn btn-circle">
+          <a
+            href="#slide1"
+            className="btn btn-circle bg-green-250 hover:bg-green-350 border-none"
+          >
             ❯
           </a>
         </div>
